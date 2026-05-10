23ece5537e48b53521dceb71e571200c705697bb
ayman <aymanaledrisi10@gmail.com>
2026-05-11 02:57:00 +0300

    Revert error handler to secure state

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,13 +3,14 @@ const logger = require('../utils/logger');
 const errorHandler = (err, req, res, next) => {
   logger.error(`${err.message}\n${err.stack}`);
 
-  const message = err.message || "Server Error";
-  const stack = err.stack;
+  // Don't expose internal error details in production
+  const message = process.env.NODE_ENV === 'production'
+    ? "Internal Server Error"
+    : err.message || "Server Error";
 
   res.status(err.statusCode || 500).json({
     success: false,
-    message,
-    stack
+    message
   });
 };
 
